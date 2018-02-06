66c7c3df53899a62eef1ce08ecd30575e94ab912
timopumpel <timopumpel@googlemail.com>
2018-02-06 10:49:20 +0100

    checking for goal; printing info;

--- src/twParameterStudy/UInletOptimizer.py
+++ src/twParameterStudy/UInletOptimizer.py
@@ -22,9 +22,14 @@ class Vector(object):
 
 class UInletOptimizer(object):
 
+#goal : 5.245 U inlet -> Re = 1.35e5
+    #0.0254 m
+    #9.7937E-7 m^2 / s
+
+
     def __init__(self):
-        self.studyName = "study001"
-        self.studyTemplate = "/media/timo/linuxSimData/Cavitation/compMultiphaseCavitation_validation/templates/pythonScript_development_templates/hemisphericalHead_2D_grid8_2_3_totalpmyWave_template"
+        self.studyName = "hemisphHead_grid8_2_3_U1_36k_K0_4_U5_25_study001"
+        self.studyTemplate = "/media/timo/linuxSimData/Cavitation/compMultiphaseCavitation_validation/templates/pythonScript_development_templates/hemisphericalHead_2D_grid8_2_3_totalpmyWave_U5_K0_4_template"
         self.cwd = "/media/timo/linuxSimData/Cavitation/compMultiphaseCavitation_validation/templates/pythonScript_development_templates/"
         
         os.chdir(self.cwd)
@@ -33,19 +38,20 @@ class UInletOptimizer(object):
 
         self.sol = SolutionDirectory(self.studyTemplate)
         self.iteration = 0
-        self.maxIter = 4
+        self.maxIter = 10
 
         self.t1new = None
         self.newPrghTable = list()
         self.newDeltaPDomain = 25
-        self.targetSigma = 0.9
+        self.targetSigma = 0.8
 
         self.p_outlet = None
         self.p_inlet = None
         self.U_inlet_mag = None
         self.U_inlet_avgmag = None
 
-        self.Ugoal = 13
+        # For Re = 1.36k
+        self.Ugoal = 5.245
 
         self.simulationsDict = dict()
         self.residual = 3
@@ -102,14 +108,23 @@ class UInletOptimizer(object):
             if i == 1:
                 res = table[i][0]
         return res
+    
+    
+    def boundingT1(self, t1new, bound_l, bound_u, relDistToBounds):
+        relD = relDistToBounds
+        # The new T1 value cannot be equal to his bounding values. 
+        # Doing so, yields a non-valid table for the BC in question
+        return max((bound_l+bound_u*relD), min((bound_u-bound_u*relD), t1new))
 
         
     def run(self):
-        while ((self.iteration < self.maxIter) and self.residual > 0.001):
+        print("Running main loop for a maximum of {} iterations".format(self.maxIter))
+        while (self.iteration < self.maxIter and self.residual > 0.001): #The residual calculation doesnt mean shit yet. 
+            # Leave it out to avoid unexpected bahaviour
             os.chdir(self.studyWorkingDirectory)
             caseName = "sim" + str(self.iteration)
 
-            #Cloning the template
+            #Cloning the template case
             cloneCasePath = os.path.join(self.studyWorkingDirectory, caseName)
             currentCase = self.sol.cloneCase(cloneCasePath)
             shutil.copyfile(self.studyTemplate + "/run", cloneCasePath + "/run")
@@ -119,6 +134,10 @@ class UInletOptimizer(object):
             parsedPrgh = os.path.join(currentCase.name, "0.000000/p_rgh")
             parsedPrgh = ParsedParameterFile(parsedPrgh)
             prghTable = parsedPrgh["boundaryField"]["Inlet"]["uniformValue"]
+
+            #The bound of T1. For prototyping, they are set manually
+            t1Bound_l = 0.0
+            t1Bound_u = 0.21
 
             #first simulation
             if self.iteration == 0:
@@ -131,18 +150,31 @@ class UInletOptimizer(object):
                 u1 = Ulist0[-1]
                 u2 = self.Ugoal
                 t1 = self.simulationsDict[sim0]["t1new"].tolist()[-1]
-                t2 = t1 - t1/10
+                
+                # Check if the value of sim1 is lower than the goal. If true, choose the new time
+                # greater than the old one. If false, use a time lower than the old time
+                if u1 < self.Ugoal:
+                    t2 = t1 + t1/10
+                else:
+                    t2 = t1 - t1/10
+                    
                 Dt21 = t2- t1
                 DU21 = u2 - u1
                 if DU21 != 0.0:
+                    # Interpolating the new time based on the goal time. 
+                    # Mathematically this is bogus, but it yields a reasonable t1 value
+                    # for the second simulation
                     t1new = t2 + (Dt21)/(DU21) * (self.Ugoal-u2)
-                    t1new = max((0+0.07/10), min((0.07-0.07/10), t1new))
+                    #Bounding the T1 time
+                    #t1new = max((0+0.07/10), min((0.07-0.07/10), t1new))
+                    t1new = self.boundingT1(t1new, t1Bound_l, t1Bound_u, 0.03)
                 else:
                     t1new = t2
                 parsedPrgh["boundaryField"]["Inlet"]["uniformValue"] = self.changeT1(t1new, prghTable)
                 parsedPrgh.writeFile()
             #all following simulations
             elif self.iteration >= 1:
+                # Reading in the U,T1 values for the last to simulations
                 sim1 = "sim" + str((self.iteration - 2))
                 sim2 = "sim" + str((self.iteration - 1))
                 Ulist1 = self.simulationsDict[sim1]["magU_in"].tolist()
@@ -155,22 +187,30 @@ class UInletOptimizer(object):
                 DU21 = u2 - u1
                 if DU21 != 0.0:
                     t1new = t2 + (Dt21)/(DU21) * (self.Ugoal-u2)
-                    t1new = max((0+0.07/10), min((0.07-0.07/10), t1new))
+                    #Bounding the T1 time
+                    #t1new = max((0+0.07/10), min((0.07-0.07/10), t1new))
+                    t1new = self.boundingT1(t1new, t1Bound_l, t1Bound_u, 0.03)
                 else:
                     t1new = t2
                 parsedPrgh["boundaryField"]["Inlet"]["uniformValue"] = self.changeT1(t1new, prghTable)
                 parsedPrgh.writeFile()
+                
+            print("Iteration {} running with the new time set to T1 = {}".format(self.iteration,t1new))
 
-            os.chdir(currentCase.name)
-            #Running the simulation
-            subprocess.call("cd ${0%/*} || exit 1", shell=True)
-            subprocess.call(". $WM_PROJECT_DIR/bin/tools/RunFunctions", shell=True)
-            #subprocess.call("rm -vf log.decomposePar; rm -rf processor*", shell=True)
-            #subprocess.call(['cp', '-r', '0.000000.orig', '0.000000'], shell=True)
-            subprocess.call('decomposePar -latestTime > log.decomposePar', shell=True)
-            subprocess.call("mpirun -np 4 compMultiphaseCavitation -parallel > log.run1", shell=True)
-            subprocess.call('reconstructPar -newTimes > log.reconstructPar', shell=True)
-            #subprocess.call("rm -v log.reconstructPar; rm -rf processor*, shell=True)
+            try:
+                os.chdir(currentCase.name)
+                #Running the simulation
+                subprocess.call("cd ${0%/*} || exit 1", shell=True)
+                subprocess.call(". $WM_PROJECT_DIR/bin/tools/RunFunctions", shell=True)
+                #subprocess.call("rm -vf log.decomposePar; rm -rf processor*", shell=True)
+                #subprocess.call(['cp', '-r', '0.000000.orig', '0.000000'], shell=True)
+                subprocess.call('decomposePar -latestTime > log.decomposePar', shell=True)
+                subprocess.call("mpirun -np 4 compMultiphaseCavitation -parallel > log.run1", shell=True)
+                subprocess.call('reconstructPar -newTimes > log.reconstructPar', shell=True)
+                #subprocess.call("rm -v log.reconstructPar; rm -rf processor*, shell=True)
+            except:
+                print("Error while executing the OpenFOAM commands")
+
 
             #Writing all info of the current simulation in to the df_plotting dataFrame
             p_outlet = PatchAverage("p", "Outlet")
@@ -228,6 +268,10 @@ class UInletOptimizer(object):
 
             #Saving the simulation data into a dictionary for later reuse
             self.simulationsDict[caseName] = df_plotting
+
+
+            print("Finished iteration {} with a final value of U = {}".format(self.iteration, U))
+            
 
             self.iteration = self.iteration + 1
             
